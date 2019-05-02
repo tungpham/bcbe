dc786d70e049fb0ea469cd1ac22efa349edc0c20
Tung Pham <phatung@amazon.com>
2019-05-01 23:22:31 -0500

    update url

--- src/main/java/com/tung/bcbe/controller/GenContractorController.java
+++ src/main/java/com/tung/bcbe/controller/GenContractorController.java
@@ -5,18 +5,19 @@ import com.tung.bcbe.repository.GenContractorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
-//@RequestMapping("/gencontractors")
+@RequestMapping("/gencontractors")
 public class GenContractorController {
 
     @Autowired
     GenContractorRepository genContractorRepository;
 
-    @PostMapping("/gencontractors")
+    @PostMapping
     public GenContractor createGeneralContractor(@Valid @RequestBody GenContractor genContractor) {
         return genContractorRepository.save(genContractor);
     }
---
+++ src/main/java/com/tung/bcbe/model/Qualification.java
@@ -0,0 +1,26 @@
+package com.tung.bcbe.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.PersistenceConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@RequiredArgsConstructor(onConstructor = @__(@PersistenceConstructor))
+@AllArgsConstructor
+@Builder
+@Data
+@Entity(name = "proposal")
+@Table(name = "proposal")
+public class Qualification extends ID {
+    
+    @Column
+    private String name;
+    
+    @Column
+    private String description;
+}
