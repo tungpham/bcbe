7657972770697591183ae9aaabaca7ffa4a43829
Tung Pham <phatung@amazon.com>
2019-05-05 23:44:26 -0500

    add origin

--- src/main/java/com/tung/bcbe/controller/TemplateController.java
+++ src/main/java/com/tung/bcbe/controller/TemplateController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.UUID;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/templates")
 @Slf4j
