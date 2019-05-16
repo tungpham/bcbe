39dabe9b69eaff01563f492119d430647cda59c0
Tung Pham <phatung@amazon.com>
2019-05-15 22:46:20 -0500

    add download

--- src/main/java/com/tung/bcbe/controller/ProjectController.java
+++ src/main/java/com/tung/bcbe/controller/ProjectController.java
@@ -2,6 +2,8 @@ package com.tung.bcbe.controller;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.google.common.io.ByteStreams;
 import com.tung.bcbe.model.Project;
 import com.tung.bcbe.model.ProjectFile;
 import com.tung.bcbe.repository.ContractorRepository;
@@ -12,26 +14,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/contractors")
 @Slf4j
 public class ProjectController {
 
@@ -50,7 +56,7 @@ public class ProjectController {
     @Value("${S3_BUCKET}")
     private String bucket;
 
-    @PostMapping("/{gen_id}/projects")
+    @PostMapping("/contractors/{gen_id}/projects")
     public Project createProject(@PathVariable(value = "gen_id") UUID genId,
                                  @Valid @RequestBody Project project) {
         return contractorRepository.findById(genId).map(genContractor -> {
@@ -59,7 +65,7 @@ public class ProjectController {
         }).orElseThrow(Util.notFound(genId));
     }
 
-    @GetMapping("/{gen_id}/projects")
+    @GetMapping("/contractors/{gen_id}/projects")
     public Page<Project> getProjectsByGenContractor(@PathVariable(value = "gen_id") UUID genId, Pageable pageable) {
         return projectRepository.findByContractorId(genId, pageable);
     }
@@ -94,6 +100,7 @@ public class ProjectController {
         });
     }
     
+    @Transactional
     @DeleteMapping("/projects/{project_id}/files/{file_name}")
     public void deleteFile(@PathVariable(value = "project_id") UUID projectId, 
                            @PathVariable(value = "file_name") String fileName) {
@@ -102,5 +109,31 @@ public class ProjectController {
             s3.deleteObject(bucket, key);
             projectFileRepository.deleteByName(fileName);
         });
+    }
+    
+    @GetMapping("/projects/{project_id}/files/{file_name}")
+    public ResponseEntity<byte[]> download(@PathVariable(value = "project_id") UUID projectId,
+                                           @PathVariable(value = "file_name") String fileName) throws IOException {
+        String key = projectId + "/" + fileName;
+        S3Object s3Object = s3.getObject(bucket, key);
+        InputStream is = s3Object.getObjectContent();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ByteStreams.copy(is, baos);
+        is.close();
+        
+        return ResponseEntity.ok().contentType(contentType(fileName))
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + fileName + "\"")
+                .body(baos.toByteArray());
+    }
+
+    private MediaType contentType(String keyname) {
+        String[] arr = keyname.split("\\.");
+        String type = arr[arr.length-1];
+        switch(type) {
+            case "txt": return MediaType.TEXT_PLAIN;
+            case "png": return MediaType.IMAGE_PNG;
+            case "jpg": return MediaType.IMAGE_JPEG;
+            default: return MediaType.APPLICATION_OCTET_STREAM;
+        }
     }
 }
