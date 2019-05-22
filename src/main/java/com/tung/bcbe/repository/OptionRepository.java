b4c011cc957a2f2af9b8f67d0e170f456128e23a
Tung Pham <phatung@amazon.com>
2019-05-22 01:11:51 -0500

    fix

--- src/main/java/com/tung/bcbe/repository/OptionRepository.java
+++ src/main/java/com/tung/bcbe/repository/OptionRepository.java
@@ -2,8 +2,10 @@ package com.tung.bcbe.repository;
 
 import com.tung.bcbe.model.Option;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.UUID;
 
+@CrossOrigin(origins = "*")
 public interface OptionRepository extends PagingAndSortingRepository<Option, UUID> {
 }
