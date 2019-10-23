53c12ebc89f688ce82b76527df72502fb67bb491
Tung Pham <phatung@amazon.com>
2019-10-23 10:27:35 -0500

    add cities

--- src/main/java/com/tung/bcbe/controller/SpecialtyController.java
+++ src/main/java/com/tung/bcbe/controller/SpecialtyController.java
@@ -16,33 +16,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/specialties")
 public class SpecialtyController {
-    
+
     @Autowired
     private SpecialtyRepository specialtyRepository;
-    
+
     @PostMapping
     public Specialty add(@RequestBody @Valid Specialty specialty) {
         return specialtyRepository.save(specialty);
     }
-    
+
     @GetMapping("/{spec_id}")
     public Specialty get(@PathVariable(name = "spec_id") UUID specId) {
         return specialtyRepository.findById(specId).orElseThrow(Util.notFound(specId, Specialty.class));
     }
-    
+
     @GetMapping
     public Page<Specialty> getAll(Pageable pageable) {
         return specialtyRepository.findAll(pageable);
     }
-    
+
     @PutMapping("/{spec_id}")
-    public Specialty edit(@PathVariable(name = "spec_id") UUID specId, 
+    public Specialty edit(@PathVariable(name = "spec_id") UUID specId,
                           @RequestBody @Valid Specialty specialty) {
         return specialtyRepository.findById(specId).map(spec -> {
             if (specialty.getName() != null) {
@@ -57,9 +59,14 @@ public class SpecialtyController {
             return specialtyRepository.save(spec);
         }).orElseThrow(Util.notFound(specId, Specialty.class));
     }
-    
+
     @DeleteMapping("/{spec_id}")
     public void delete(@PathVariable(name = "spec_id") UUID specId) {
         specialtyRepository.deleteById(specId);
+    }
+
+    @GetMapping("/cities")
+    public List<String> getCities() {
+        return Arrays.asList("ha noi", "sai gon", "hai phong", "ho chi minh", "da nang", "nha trang", "phu quoc", "vung tau", "hue", "hoi an");
     }
 }
