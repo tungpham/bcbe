19a01e1c7272bb97186784ebe573884953578a74
Tung Pham <phatung@amazon.com>
2020-01-08 00:48:26 -0600

    fix

--- src/main/java/com/tung/bcbe/config/SecurityConfig.java
+++ src/main/java/com/tung/bcbe/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.tung.bcbe.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -12,8 +12,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoderJwkSupport;
 
-//@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfig {
 
     @Value("${auth0.audience}")
     private String audience;
